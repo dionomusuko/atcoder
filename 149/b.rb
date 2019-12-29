68c9d6e5924b1923db791fe143f9857849026684
dionomusuko <dandybird1017@gmail.com>
2019-12-29 23:13:43 +0900

    comp b

--- 149/b.rb
+++ 149/b.rb
@@ -1,11 +1,15 @@
 a, b, k = gets.split.map(&:to_i)
 
-k.times do
-  if a >= 1
-    a-=1
-  elsif b >= 1
-    b-=1
+if a >= k
+  a -= k
+else
+  k -= a
+  a = 0
+  if b >= k
+    b -= k
   else
+    k -= b
+    b = 0
   end
 end
 
